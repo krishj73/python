ea6e1c9523c72b678d905bae00da93bf513d490f
krishj73 <krishjaisingh73@gmail.com>
2025-01-06 23:38:23 +0530

    New File

--- 1-loops.py
+++ 1-loops.py
@@ -12,10 +12,39 @@ while(i<11):
     i+=1
 print()
 
-#do while
-i=0
-while True:
-    print(i, end=" ")
-    i+=1
-    if (i>10):
-        break
+print("patterns using loop : ")
+#1
+n=5
+for i in range(1, n+1):
+    print("*"*i)
+print()
+
+#2
+n=5
+for i in range(n, 0, -1):
+    print("*"*i)
+print()
+
+#3
+n=5
+for i in range(1, n+1):
+    print(' ' * (n-i) + '*' * (2*i-1))
+print()
+#loop executed from 1 to n+1 (n)
+#[' ' x (n-i)] determines no. of spaces each side, n is constant and i is inc value
+#['*' x (2i - 1) ] determines no. of stars printed, ex:2x1-1 = 1 star printed, 2x2-1 = 3 stars printed
+
+#4
+n=5
+#upper part
+for i in range(1, n+1):
+    print(' ' * (n-i) + '*' * (2*i-1))
+#lower part
+for i in range(n-1, 0, -1):
+    print(' ' * (n-i) + '*' * (2*i-1))
+print()
+
+#5 - square
+n=5
+for i in range(n):
+    print('*'*n)
