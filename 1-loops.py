27266afd43c15c4dd32b88ba1f89835aa9cdf841
krish <krishjaisingh73@gmail.com>
2026-07-10 23:06:25 +0530

    New File

--- 1-loops.py
+++ 1-loops.py
@@ -1,37 +1,37 @@
 #for
 print("for loop : ")
-for i in range(0,11):
-    print(i, end=" ")
+for i in range(0, 11):
+    print(i, end = " ")
 print()
 
 #while
 print("while loop : ")
-i=0
-while(i<11):
+i = 0
+while (i<11):
     print(i, end=" ")
-    i+=1
+    i += 1
 print()
 
 #do while
 #i=0
 #while True:
-#    print(i, end=" ")
-#    i+=1
+#    print(i, end = " ")
+#    i += 1
 #    if (i>10):
 #        break
 #print()
 
 print("patterns using loop : ")
 #1
-n=5
+n = 5
 for i in range(1, n+1):
-    print("*"*i)
+    print("*" * i)
 print()
 
 #2
 n=5
 for i in range(n, 0, -1):
-    print("*"*i)
+    print("*" * i)
 print()
 
 #3
@@ -56,4 +56,4 @@ print()
 #5 - square
 n=5
 for i in range(n):
-    print('*'*n)
+    print('*' * n)
