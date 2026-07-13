7f45c967a260cc18b4faf53508e37d9ee4b6d7a1
krish <krishjaisingh73@gmail.com>
2026-07-13 23:07:30 +0530

    New File

--- 2-if.py
+++ 2-if.py
@@ -1,15 +1,13 @@
 #if/else
 age = int(input("Enter user age : "))
 if age<18:
-    print("Chhoti bacchi ho kya")
+    print("Chhoti bacchi ho kya")       #you're not an adult
 elif age==18:
-    print("Ab majaa ayega na bhidu")
+    print("Ab majaa ayega na bhidu")    #you're an adult
 else:
-    print("Tu toh bada ho gaya")
+    print("Tu toh bada ho gaya")        #you're more than an adult
 
-#nested if/else
 a, b, c = 10, 20, 30
-
 if a>b and a>c:
     greatest = a
 elif b>a and b>c:
@@ -18,3 +16,15 @@ else:
     greatest = c
 
 print("Greatest number :", greatest)
+
+#nested if/else
+ntn = input("enter nationality : ")
+age = int(input("enter age : "))
+
+if ntn.lower() == "indian":
+    if age > 18:
+        print("you're eligible to vote in india")
+    else:
+        print("you're inelegible to vote in india")
+else:
+    print("you're not an indian citizen")
